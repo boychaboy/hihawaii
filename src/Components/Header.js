be89b4cca617d9e7388e0a451ce881a26cd5d585
boychaboy <hoon2j@gmail.com>
2020-05-02 17:05:39 +0900

    header mod

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -24,10 +24,10 @@ class Header extends Component {
          <ul id="nav" className="nav">
             <li className="current"><a className="smoothscroll" href="#home">Home</a></li>
             <li><a className="smoothscroll" href="#about">About</a></li>
-	         <li><a className="smoothscroll" href="#resume">Resume</a></li>
             <li><a className="smoothscroll" href="#portfolio">Works</a></li>
-            <li><a className="smoothscroll" href="#testimonials">Testimonials</a></li>
-            <li><a className="smoothscroll" href="#contact">Contact</a></li>
+            <li><a className="smoothscroll" href="#artworks">Arts</a></li>
+            <li><a className="smoothscroll" href="#photos">Images</a></li>
+            <li><a className="smoothscroll" href="#contact">Information</a></li>
          </ul>
 
       </nav>
@@ -45,7 +45,7 @@ class Header extends Component {
       <p className="scrolldown">
          <a className="smoothscroll" href="#about"><i className="icon-down-circle"></i></a>
       </p>
-
+      
    </header>
     );
   }
