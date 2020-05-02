e5d1b113daa4be2d8bd79b80e89520277bacfdf0
boychaboy <hoon2j@gmail.com>
2020-05-02 18:01:03 +0900

    social link

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -9,7 +9,7 @@ class Header extends Component {
       var description= this.props.data.description;
       var city= this.props.data.address.city;
       var networks= this.props.data.social.map(function(network){
-        return <li key={network.name}><a href={network.url}><i className={network.className}></i></a></li>
+        return <li key={network.name}><a href={network.url}><i className={network.className} aria-hidden="true"></i></a></li>
       })
     }
 
@@ -45,7 +45,7 @@ class Header extends Component {
       <p className="scrolldown">
          <a className="smoothscroll" href="#about"><i className="icon-down-circle"></i></a>
       </p>
-      
+
    </header>
     );
   }
